7949626a45c1c6227394cc0e6d97f5a2170bb9b7
Oleksandr Kalabin <oleksandr.kalabin@student.tuke.sk>
2021-02-20 16:23:04 +0200

    main project files added

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+let Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://firebirdsql.org/file/about/firebird-logo-300.png'/>
+        </header>
+    )
+}
+
+export default Header;
+
---
+++ src/components/Profile.js
@@ -0,0 +1,31 @@
+let Profile = () =>{
+    return (<div className='content'>
+        <div>
+            <img
+                src='https://images.ctfassets.net/hrltx12pl8hq/6bi6wKIM5DDM5U1PtGVFcP/1c7fce6de33bb6575548a646ff9b03aa/nature-photography-pictures.jpg?fit=fill&w=800&h=300'/>
+        </div>
+
+        <div>
+            ava+deskr
+        </div>
+
+        <div>
+            MyPosts
+            <div>
+                New Post
+            </div>
+
+            <div>
+                <div>
+                    post 1
+                </div>
+
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    </div>);
+}
+
+export default Profile;
